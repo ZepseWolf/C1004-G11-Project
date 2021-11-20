e6d962d95fb8ac389103d12364861eafc4bcc750
Zepse Wolf <douglasmeows@gmail.com>
2021-11-20 21:14:21 +0800

    Added working blinker with sequence .

--- index.c
+++ index.c
@@ -5,7 +5,7 @@
 const int ledPin = 18; // Regular LED - Broadcom pin 23, P1 pin 16
 const int butPin = 17; // Active-low button - Broadcom pin 17, P1 pin 11
 
-void blink(int *freq);
+void checkForInterupt(int *stop);
 
 int main(void)
 {   
@@ -15,40 +15,46 @@ int main(void)
     pinMode(butPin, INPUT);      // Set button as INPUT
     pullUpDnControl(butPin, PUD_UP); 
     
-    
+    printf("Input blink frequency in ms: ");
+    scanf("%d", &freq);
+    printf("Input blink frequency in ms: ");
+    scanf("%d", &freq);
+
     while(1)
     {   delay(10);
         if (digitalRead(butPin)) // Button is released if this returns 1
         {
             if(holding == 1){
-                // click   
-                printf("\n Clicking");
+                // Click function   
+               
                 if(count == 1 ){
-                    printf("\n start c1 "  );
-                    digitalWrite(ledPin, HIGH); 
-                    delay(freq); 
-                    digitalWrite(ledPin, LOW); 
-                    delay(freq); 
-                    printf("\n end c1 "  );
-                    
+                    printf("\n=== Init first sequence ===");
+                    digitalWrite(ledPin, HIGH);
                 }
                 else if(count == 2){
-                    printf("\n start c2 "  );
-                    digitalWrite(ledPin, HIGH);
-                    printf("\n end c2 "  );
-                //     count = 3;
-                    // count++;
+                    printf("\n=== Init second sequence ===");
+                    digitalWrite(ledPin, LOW);
                 }
                 else if(count == 3){
-                    printf("\n start c3 "  );
-                    digitalWrite(ledPin, LOW);
-                    printf("\n end c3 "  ); 
+                    printf("\n=== Init second sequence (Long button press to go to go to next sequence) ==="  );
+                    int stop = 1;
+                    while (stop)
+                    {   
+                        delay(10); 
+                        checkForInterupt(&stop);
+                        digitalWrite(ledPin, HIGH); 
+                        checkForInterupt(&stop);
+                        delay(freq); 
+                        checkForInterupt(&stop);
+                        digitalWrite(ledPin, LOW);
+                        checkForInterupt(&stop);
+                        delay(freq); 
+                        checkForInterupt(&stop);
+                    }
                     count = 0;
                 }
-                
-            printf("\n Button clicked! , count:%d and holding : %d" ,count,holding );
-            count++;
-            holding = 0;
+                count++;
+                holding = 0;
             }
         }
         else // If digitalRead returns 0, button is pressed
@@ -60,9 +66,8 @@ int main(void)
     return 0;
 }
 
-void blink(int *freq){
-    digitalWrite(ledPin, HIGH); 
-    delay(*freq); 
-    digitalWrite(ledPin, LOW); 
-    delay(*freq); 
+void checkForInterupt(int *stop){
+    if (!digitalRead(butPin)){
+        *stop = 0;
+    }
 }
